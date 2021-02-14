7513883efa1cc059bcedcbc5121205efb302cae5
Stefano Colamonaco <juststep.2000@gmail.com>
2021-02-14 23:53:30 +0100

    Definition in pcb.h and asl.h

--- asl.h
+++ asl.h
@@ -1,3 +1,9 @@
 #include <pandos_const.h>
 #include <pandos_types.h>
 
+/* ASL management */
+int insertBlocked(int *semAdd,pcb_t *p);                            /* */
+pcb_t* removeBlocked(int *semAdd);
+pcb_t* outBlocked(pcb_t *p);
+pcb_t* headBlocked(int *semAdd);
+void initASL();
--- pcb.h
+++ pcb.h
@@ -15,27 +15,29 @@ extern pcb_t pcbFree_table[MAXPROC];
 
 
 
-/* Fills pcbFree_h list */
-void initPcbs();
+/* PCB allocation */
+
+void initPcbs();                                                    /* Fills pcbFree_h list */
+void freePcb(pcb_t * p);                                            /* Insert a PCB in pcbFree_h list */
+pcb_t *allocPcb();                                                  /* PCB allocation and initialization*/
+
+/* PCB list */
+
+pcb_t* mkEmptyProcQ();                                              /* Make an empty PCB list */
+int emptyProcQ(pcb_t *tp);                                          /* Returns TRUE if the list is empty, FALSE otherwise */
+void insertProcQ(pcb_t **tp,pcb_t* p);                              /* Insert PCB p at the end of tp list */
+pcb_t *headProcQ(pcb_t **tp);                                       /* Return the element at the end of tp ( NULL otherwise ) */
+pcb_t* removeProcQ(pcb_t **tp);                                     /* Removes oldest element from tp and return a pointer */
+pcb_t* outProcQ(pcb_t **tp, pcb_t*p);                               /* Removes PCB p from tp list ( return NULL otherwise ) */
+
+/* PCB threes */
+
+int emptyChild(pcb_t *p);                                           /* Returns TRUE if p has no childrens, FALSE otherwise */
+void insertChild(pcb_t*prnt,pcb_t *p);                              /* Insert p as child of prnt  */
+pcb_t* removeChild(pcb_t *p);                                       /* Removes firs child of p ( return NULL otherwise ) */
+pcb_t *outChild(pcb_t* p);                                          /* Removes P from the father's children */
 
 
-/*
-void initPcbs() {
-    pcb_t *sentinel, *tmp;
-    pcbFree_h = sentinel;
-    *sentinel = pcbFree_table[0];
-    *tmp = pcbFree_table[0];
-    for (int i = 1; i<MAXPROC; i++) {
-        *tmp->p_next = pcbFree_table[i];
-        *tmp = pcbFree_table[i];
-        *tmp->p_prev = pcbFree_table[i-1];
-    }
-    *tmp = pcbFree_table[MAXPROC-1];
-    *tmp->p_next = pcbFree_table[0];
-    *tmp = pcbFree_table[0];
-    *tmp->p_prev = pcbFree_table[MAXPROC-1];
-}
-*/
 
 
 #endif //PROGETTOSO20_21_PCB_H
