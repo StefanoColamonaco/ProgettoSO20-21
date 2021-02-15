964ca8617e24532f39d4b2ab78bce78121d6c611
Stefano Colamonaco <juststep.2000@gmail.com>
2021-02-15 10:09:50 +0100

    some adjustments in pcb.h

--- asl.h
+++ asl.h
@@ -1,9 +1,18 @@
 #include <pandos_const.h>
 #include <pandos_types.h>
 
+/*Table of free semaphores*/
+//extern semd_table[MAXPROC];
+
+/* Pointer to list of free semaphores*/
+//extern semdFree_h;                                        // [Attenzione] manca il tipo del semaforo da definire
+
+/* Pointer to list of active semaphores */
+//extern semd_h
+
 /* ASL management */
-int insertBlocked(int *semAdd,pcb_t *p);                            /* */
-pcb_t* removeBlocked(int *semAdd);
-pcb_t* outBlocked(pcb_t *p);
-pcb_t* headBlocked(int *semAdd);
-void initASL();
+int insertBlocked(int *semAdd,pcb_t *p);                            /* Insert p into the queue of blocked processes associated to semAdd */
+pcb_t* removeBlocked(int *semAdd);                                  /* Returns the first PCB from the queue of blocked processes */
+pcb_t* outBlocked(pcb_t *p);                                        /* Remove p from the queue where it is blocked ( returns NULL otherwise ) */
+pcb_t* headBlocked(int *semAdd);                                    /* Returns the PCB at the head of semAdd's queue */
+void initASL();                                                     /* Fills semdFree list */
