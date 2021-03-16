0ec708fb14f87a623df8526b54a2b723964c3998
andrea-corradetti <andreacorradetti28@gmail.com>
2021-03-16 14:35:19 +0100

    Declared new global variables

--- src/init.c
+++ src/init.c
@@ -1,1 +1,17 @@
-//vedere .h
+#include "init.h"
+
+
+int processCount = 0;        //number of started but not yet terminated processes
+
+int softBlockCount = 0;      //number of processes blocked due to I/O
+
+pcb_t *readyQueue = mkEmptyProcQ();       //tail pointer to queue of ready processes
+
+pcb_t *currentProcess = NULL;   //pointer to pcb that is in running state
+
+//todo populate pass-up vector
+
+int main() {
+    initPcbs();
+    initASL();
+}
--- src/init.h
+++ src/init.h
@@ -1,1 +1,27 @@
-// serve a gestire l'inizializzazione di variabili globali e strutture dati
+
+#ifndef PROGETTOSO20_21_INIT_H
+#define PROGETTOSO20_21_INIT_H
+
+
+#include "pandos_types.h"
+#include "pandos_const.h"
+#include "pcb.h"
+#include "asl.h"
+
+extern int processCount;        //number of started but not yet terminated processes
+
+extern int softBlockCount;      //number of processes blocked due to I/O
+
+extern pcb_t *readyQueue;       //tail pointer to queue of ready processes
+
+extern pcb_t *currentProcess;   //pointer to pcb that is in running state
+
+extern semd_t deviceSemaphores[];
+
+
+//todo add device semaphores
+
+
+
+
+#endif //PROGETTOSO20_21_INIT_H
