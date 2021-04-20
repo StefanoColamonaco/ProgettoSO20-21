f89da58313db952df7dd2b16a93bb64d7ba2f713
andrea-corradetti <andreacorradetti28@gmail.com>
2021-04-20 22:42:58 +0200

    [WIP] Added initUprocPageTable

--- src/vmSupport.c
+++ src/vmSupport.c
@@ -5,5 +5,37 @@
 swap_t swapPool[POOLSIZE]; 
 semd_t *swapSem;
 
+unsigned int freeAsidBitmap = 0b11111111;   
 
 
+void initUprocPageTable(pcb_t *uproc) {
+    int asid = uproc->p_supportStruct->sup_asid;
+    int vpn;
+    for (int i = 0; i < USERPGTBLSIZE; i++) {
+        vpn = getVPNAddress(i);
+        unsigned int entryHI = vpn << VPNSHIFT + asid << ASIDSHIFT;
+        unsigned int entryLO = DIRTYON;
+        uproc->p_supportStruct->sup_privatePgTbl[i].pte_entryHI = entryHI;
+        uproc->p_supportStruct->sup_privatePgTbl[i].pte_entryLO = entryLO;
+    }
+}
+
+/*check bitmap for free Asid. Returns -1 if none is free*/
+int getFreeAsid() {
+    for (int i = 1; i <= 8; i++) {
+        if ( ((freeAsidBitmap >> i) & 1U) == ON )
+            return i;
+    }
+    return -1;
+}
+
+
+int getVPNAddress(int index) {
+    if(index < 0 || index > 31) {
+        //TODO handle exception
+    }
+    if (index == 31) {
+        return 0xBFFFF; 
+    }
+    return 0x80000 + index;
+}
